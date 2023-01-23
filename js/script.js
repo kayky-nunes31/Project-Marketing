e3e0ff004d34bbb0d6f82ac5d563f6d8057f71c1
Kayky Hyan Nunes <kaykynunes357@gmail.com>
2023-01-23 17:41:43 -0300

    js criado

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function btt_contatos() {
+    divs_ctt.innerHTML = 
+    `
+    <div class="ctts">
+    <br>
+        <img src="imgs/icon-localização.png">
+        <h4>ENDEREÇO</h4>
+        <p>
+            Av. Paulista 332    <br>
+            Bela Vista - São Paulo - SP <br>
+            CEP - 90870-100
+        </p>
+    </div>
+    <div class="ctts">
+    <br>
+        <img src="imgs/icon-telefone.png">
+        <h4>TELEFONE</h4>
+        <p>
+            (11) 4002-8922
+        </p>
+    </div>
+    <div class="ctts">
+    <br>
+        <img src="imgs/icon-email.png">
+        <h4>E-MAIL</h4>
+        <p>
+            contato@marketeiros.com
+        </p>
+    </div>
+`;
+}
